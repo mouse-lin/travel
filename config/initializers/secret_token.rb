96203ad4062493369351225cc58cb0ca0bed748b
mouse <linhongshimouse@gmail.com>
2011-12-02 00:44:19 +0800

    create the app travel

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travel::Application.config.secret_token = 'b44639e321dbbc9bfd78c9dc91169a424dfc90023dbc48744aa35477d98c5d7be818853b08e73c0acaca7e2b7c2d955533969b8dcada1cb6b5d60bda8294d68b'
